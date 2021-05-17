cc958697eab4b1e2791445f066635a0935156d74
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-17 12:58:10 +0200

    added password authentication to /movies endpoint

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/', (req, res) => {
   res.json(responseText);
 });
 
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find().then((movies) => {
     res.status(200).json(movies);
   }).catch((err) => {
